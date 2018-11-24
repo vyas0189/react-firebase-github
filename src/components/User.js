b09d516be72166a81ca379ddda3261b394817064
Vyas Ramankulangara <vyas01890@outlook.com>
2018-11-23 22:26:44 -0600

    firebse config

--- src/components/User.js
+++ src/components/User.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import Heading from "./Heading";
 import SearchBar from "./SearchBar";
-import { auth } from "../firebase/firebaseConfig";
+import { auth } from "../firebaseConfig";
 
 class User extends Component {
   state = {
