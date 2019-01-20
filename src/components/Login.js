88c4f5da2686ec3d738597dadc8b9d504c9376fb
Vyas <vyas01890@outlook.com>
2019-01-20 16:28:54 -0600

    test

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,59 +1,60 @@
-import React, { Component } from "react";
-import { auth, firebase, db } from "../firebaseConfig";
-import { Container, Button, Icon } from "semantic-ui-react";
-import { Link } from "react-router-dom";
-import moment from "moment";
+import React, { Component } from 'react';
+import { auth, firebase, db } from '../firebaseConfig';
+import { Container, Button, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 class Login extends Component {
-  componentDidMount() {
-    this.mounted = true;
-    auth.onAuthStateChanged(user => {
-      if (this.mounted) {
-        if (user) {
-          db.collection("users")
-            .doc(`${user.uid}`)
-            .set({
-              id: user.uid,
-              name: user.displayName,
-              email: user.email,
-              photo: user.photoURL,
-              loggedInTime: moment().format("MMMM Do YYYY, h:mm:ss a")
-            })
-            .then(() => {
-              this.props.history.push("/user");
-            })
-            .catch(err => {
-              this.props.history.push("*");
-            });
-        }
-      }
-    });
-  }
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-  handleLogin = async e => {
-    e.preventDefault();
-    const provider = new firebase.auth.GithubAuthProvider();
-    await auth.signInWithPopup(provider).catch(err => console.log(err));
-  };
-  render() {
-    return (
-      <Container>
-        <Link
-          to={{
-            pathname: "/user"
-          }}
-          style={{ width: 250, display: "block", margin: "auto", padding: 10 }}
-        >
-          <Button size="big" color="black" onClick={this.handleLogin}>
-            <Icon name="github" />
-            Login with GitHub
-          </Button>
-        </Link>
-      </Container>
-    );
-  }
+	componentDidMount() {
+		this.mounted = true;
+		auth.onAuthStateChanged((user) => {
+			if (this.mounted) {
+				if (user) {
+					db
+						.collection('users')
+						.doc(`${user.uid}`)
+						.set({
+							id: user.uid,
+							name: user.displayName,
+							email: user.email,
+							photo: user.photoURL,
+							loggedInTime: moment().format('MMMM Do YYYY, h:mm:ss a')
+						})
+						.then(() => {
+							this.props.history.push('/user');
+						})
+						.catch((err) => {
+							this.props.history.push('*');
+						});
+				}
+			}
+		});
+	}
+	componentWillUnmount() {
+		this.mounted = false;
+	}
+	handleLogin = async (e) => {
+		e.preventDefault();
+		const provider = new firebase.auth.GithubAuthProvider();
+		await auth.signInWithPopup(provider).catch((err) => console.log(err));
+	};
+	render() {
+		return (
+			<Container>
+				<Link
+					to={{
+						pathname: '/user'
+					}}
+					style={{ width: 250, display: 'block', margin: 'auto', padding: 10 }}
+				>
+					<Button size="big" color="black" onClick={this.handleLogin}>
+						<Icon name="github" />
+						Login with GitHub
+					</Button>
+				</Link>
+			</Container>
+		);
+	}
 }
 
 export default Login;
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,66 +1,64 @@
-import React, { Component, lazy, Suspense } from "react";
-import { Header, Segment, Loader, Dimmer } from "semantic-ui-react";
-import axios from "axios";
-import "./Repo.css";
-const Repo = lazy(() => import("./Repo"));
+import React, { Component, lazy, Suspense } from 'react';
+import { Header, Segment, Loader, Dimmer } from 'semantic-ui-react';
+import axios from 'axios';
+import './Repo.css';
+const Repo = lazy(() => import('./Repo'));
 
 class Repos extends Component {
-  state = {
-    userRepos: []
-  };
-  componentWillMount() {
-    if (this.state.username == null) {
-      this.setState({ username: this.props.username });
-    }
-  }
-  componentDidMount() {
-    this.getRepo(this.state.username);
-  }
+	state = {
+		userRepos: []
+	};
+	componentWillMount() {
+		if (this.state.username == null) {
+			this.setState({ username: this.props.username });
+		}
+	}
+	componentDidMount() {
+		this.getRepo(this.state.username);
+	}
 
-  getRepo = async username => {
-    await axios
-      .get(`https://api.github.com/users/${username}/repos`)
-      .then(res => {
-        this.setState({
-          userRepos: res.data
-        });
-      })
-      .catch(err => {
-        console.log(err.message);
-      });
-  };
-  componentDidUpdate = prevProps => {
-    const { username: oldUserName } = prevProps;
-    const { username: newUsername } = this.props;
-    if (oldUserName !== newUsername) {
-      this.getRepo(newUsername);
-    }
-  };
+	getRepo = async (username) => {
+		await axios
+			.get(`https://api.github.com/users/${username}/repos`)
+			.then((res) => {
+				this.setState({
+					userRepos: res.data
+				});
+			})
+			.catch((err) => {
+				console.log(err.message);
+			});
+	};
+	componentDidUpdate = (prevProps) => {
+		const { username: oldUserName } = prevProps;
+		const { username: newUsername } = this.props;
+		if (oldUserName !== newUsername) {
+			this.getRepo(newUsername);
+		}
+	};
 
-  render() {
-    return (
-      <React.Fragment>
-        <Header as="h2" attached="top">
-          Repositories
-        </Header>
-        <Segment attached>
-          <div className="containerRepos">
-            <Suspense
-              fallback={
-                <Dimmer active inverted style={{ marginTop: "2em" }}>
-                  <Loader inverted content="Loading" />
-                </Dimmer>
-              }
-            >
-              {this.state.userRepos.map(repo => (
-                <Repo repo={repo} key={repo.id} />
-              ))}
-            </Suspense>
-          </div>
-        </Segment>
-      </React.Fragment>
-    );
-  }
+	render() {
+		return (
+			<React.Fragment>
+				<Header as="h2" attached="top">
+					Repositories
+				</Header>
+				<Segment attached>
+					<div className="containerRepos">
+						<Suspense
+							fallback={
+								<Dimmer active inverted style={{ marginTop: '2em' }}>
+									<Loader inverted content="Loading" />
+								</Dimmer>
+							}
+						>
+							{this.state.userRepos.map((repo) => <Repo repo={repo} key={repo.id} />)}
+						</Suspense>
+					</div>
+				</Segment>
+			</React.Fragment>
+		);
+	}
 }
 
 export default Repos;
